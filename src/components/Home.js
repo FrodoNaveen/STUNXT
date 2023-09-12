be681d5a5168dec305ea730eb47597e8f6acb89e
Kamesh <ka72kamesh@gmail.com>
2023-09-12 13:23:53 +0530

    Added menubar, home

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import Time from "./Time";
+import MenuBar from "./MenuBar";
+
+
+const Home = () => {
+
+
+return (
+    <div className="container card mt-5 phoneSize">
+      <Time />
+      <MenuBar/>
+      </div>
+);
+
+
+}; 
+export default Home;
---
+++ src/components/MenuBar.js
@@ -0,0 +1,91 @@
+import profileicon from "../images/Menubar/Ellipse 19.png";
+import searchicon from "../images/Menubar/search.png";
+import notificationicon from "../images/Menubar/notifications.png";
+import forum from "../images/Menubar/forum.png";
+import proirityhigh from "../images/Menubar/priority_high.png";
+import greenicon from "../images/Menubar/green icon.png";
+
+const MenuBar = () => {
+  return (
+    <nav className="navbar bg-white navbar-white ">
+      <div className="container-fluid ">
+        <div className="d-flex justify-content-start align-items-center position-relative">
+          <div style={{ marginRight: "10px" }}></div>
+
+          {/* Profile Icon (Left) */}
+          <a className="navbar-profile" href="#">
+            <img
+              src={profileicon}
+              alt="Avatar Logo"
+              style={{ width: "40px" }}
+              className="rounded-pill"
+            />
+            {/* Priority High Icon (Inside Profile Icon) */}
+            <div
+              className="priority-icon"
+              style={{
+                position: "absolute",
+                bottom: "0",
+                right: "0",
+                backgroundColor: "#f2f2f2", // Gray background color
+                borderRadius: "50%", // Make it round
+                width: "15px", // Adjust the width and height as needed
+                height: "15px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <img
+                src={proirityhigh}
+                alt="Priority High"
+                style={{
+                  filter: "brightness(0)", // Make the icon black
+                  width: "5px", // Adjust the icon size
+                }}
+              />
+            </div>
+          </a>
+        </div>
+        <div className="d-flex justify-content-end align-items-center">
+          {/* Search Icon */}
+          <a className="navbar-search" href="#">
+            <img src={searchicon} alt="Search Logo" />
+          </a>
+          <div style={{ marginRight: "20px" }}></div>
+          {/* Notification Icon */}
+          <a className="navbar-notification" href="#">
+            <img src={notificationicon} alt="Notify Logo" />
+          </a>
+          <div style={{ marginRight: "20px" }}></div>
+          {/* Forum Icon */}
+          <a className="navbar-forum" href="#" style={{ position: "relative" }}>
+            <img src={forum} alt="Forum Logo" />
+            {/* Green Round Icon (Top Right Inside Forum Icon) */}
+            <div
+              className="green-icon"
+              style={{
+                position: "absolute",
+                top: "-10px", // Adjust the positioning as needed
+                right: "5px", // Adjust the positioning as needed
+              }}
+            >
+              <img
+                src={greenicon}
+                alt="Green Icon"
+                style={{
+                  borderRadius: "50%", // Make it round
+                  width: "13px", // Adjust the width and height as needed
+                  height: "13px",
+                }}
+              />
+            </div>
+          </a>
+          <div style={{ marginRight: "-10px" }}></div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default MenuBar;
