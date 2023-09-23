b2f70b4d280abf32d2c566f5b412546abaf1dcc0
FrodoNaveen <naveen@Kavins-MacBook-Pro.local>
2023-09-23 09:01:33 +0530

    chatbox created

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,14 +1,35 @@
 import Time from "./Time"
 import refreshlogo from "../images/Home/refreshlogo.svg";
 import { useNavigate } from "react-router-dom";
+import ChatBoxNavbar from "./ChatBoxNavbar";
+// import { RxAvatar } from "react-icons/rx"
+import profileimage from "../images/Network/friendimage.svg"
+
+
 
 const ChatBox = () => {
 
     let navigate = useNavigate()
 
+    const index = 0
+
     function goBack() {
         navigate("/home")
     }
+
+    let chat = [{
+        userImage: profileimage, userName: "Frodo Naveen", userMessage: [{ message: "Hello Comedy", messageDate: "22 sep" }, { message: "Dei pombala Kamesh", messageDate: "15 Jun" },
+        { message: "Dei irukiya", messageDate: "15 Jun" }]
+    },
+    {
+        userImage: profileimage, userName: "Lionel Messi", userMessage: [{ message: "Hello Comedy", messageDate: "22 sep" }, { message: "Dei pombala Kamesh", messageDate: "15 Jun" },
+        { message: "I Won the world cup", messageDate: "15 Jun" }]
+    },
+    {
+        userImage: profileimage, userName: "CR7", userMessage: [{ message: "Hello Comedy", messageDate: "22 sep" }, { message: "Dei pombala Kamesh", messageDate: "15 Jun" },
+        { message: "Suiiiiiiii", messageDate: "15 Jun" }]
+    }
+    ]
 
     return (
         <div className="mainContainer bg-body-secondary card mt-5 phoneSize">
@@ -55,6 +76,37 @@ const ChatBox = () => {
                     </div>
                 </div>
             </div>
+
+            {/* Messages in the chatbox */}
+
+            <div className="container mt-3">
+                {chat.map((ele, index) => (
+                    <div className="card borderWhite rounded-2 mt-2 cursorPointer" key={index}>
+                        <div className="row p-3">
+                            <div className="col-2">
+                                <img src={ele.userImage} alt="userimage" style={{ height: "50px", width: "50px" }} />
+                            </div>
+                            <div className="col-10">
+                                <div className="float-start">
+                                    <b>{ele.userName}</b>
+                                    <br />
+
+                                    {/* if message opened thn text muted else dark */}
+
+                                    <small>{ele.userMessage[ele.userMessage.length - 1].message}</small>
+                                </div>
+                                <div className="float-end">
+                                    <small className="text-muted">{ele.userMessage[ele.userMessage.length - 1].messageDate}</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            {/* <div style={{ marginTop: "12%", left: "80%", position: "absolute", width: "100%" }}>
+                <IoMdAddCircle size="50px" color="#00d970" />
+            </div> */}
+            <ChatBoxNavbar index={index} />
         </div >
     )
 }
